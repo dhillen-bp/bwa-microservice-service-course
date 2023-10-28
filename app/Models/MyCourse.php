bf0fab5a2d93657ffd1d6bc23b6e7b93e22b60ec
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-28 07:43:35 +0700

    create api get MyCourse & filter

--- app/Models/MyCourse.php
+++ app/Models/MyCourse.php
@@ -11,6 +11,12 @@ class MyCourse extends Model
     use HasFactory;
 
     protected $table = 'my_courses';
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
+
     protected $fillable = [
         'course_id', 'user_id',
     ];
