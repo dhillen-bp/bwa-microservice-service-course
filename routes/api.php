98ea83844777e832589210d42851cbc981b0ab93
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-28 07:38:11 +0700

    create api create MyCourse

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ImageCourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\MentorController;
-
+use App\Http\Controllers\MyCourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,3 +48,5 @@ Route::delete('lessons/{id}', [LessonController::class, 'destroy']);
 
 Route::post('image-courses', [ImageCourseController::class, 'create']);
 Route::delete('image-courses/{id}', [ImageCourseController::class, 'destroy']);
+
+Route::post('my-courses', [MyCourseController::class, 'create']);
